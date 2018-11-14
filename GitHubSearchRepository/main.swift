e8dc9a9489da79ee76baec3a8b99348ac43af0dc
masato <masato@m-mbp.local>
2018-11-14 18:41:44 +0800

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by masato on 14/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
